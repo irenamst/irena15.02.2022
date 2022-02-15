3aa57b795ae3c044ea3747277596f5ad30f37e58
Irena Staneva <irenamst@gmail.com>
2022-02-15 21:46:29 +0200

    Add files via upload

---
+++ While_Loop_Example6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loop_Example6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number less than 12 ");
+            int n = int.Parse(Console.ReadLine());
+            int factoriel = 1;
+            while (true)
+            {
+                factoriel *= n;
+                n--;
+                if (n <= 1)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("n!="+factoriel);
+        }
+    }
+}
